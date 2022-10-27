ea60cc8e0477eb30ba963d5b50a0d9b3043be40f
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-10-27 19:37:10 +0100

    Create top-level sinegen.sv module

---
+++ task1/sinegen.sv
@@ -0,0 +1,28 @@
+module sinegen #(
+    parameter ADDR_WIDTH = 8,
+              DATA_WIDTH = 8
+)(
+    input  logic                   clk,
+    input  logic                   rst,
+    input  logic                   en,
+    input  logic[(DATA_WIDTH-1):0] step_size,
+    output logic[(DATA_WIDTH-1):0] dout
+);
+
+logic[(ADDR_WIDTH-1):0] rom_addr;
+
+counter addr_counter(
+    .clk(clk),
+    .rst(rst),
+    .en(en),
+    .step_size(step_size),
+    .count(rom_addr)
+);
+
+rom sine_table(
+    .clk(clk),
+    .addr(addr),
+    .dout(dout)
+);
+
+endmodule
