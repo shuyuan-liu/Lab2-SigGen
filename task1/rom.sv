8aae1f4aa55f54737027b9cd7f1ba00269e81d06
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-10-27 18:22:47 +0100

    Populate rom.sv with code given in slides

---
+++ task1/rom.sv
@@ -0,0 +1,21 @@
+module rom #(
+    parameter ADDR_WIDTH = 8,
+              DATA_WIDTH = 8
+)(
+    input  logic                    clk,
+    input  logic [(ADDR_WIDTH-1):0] addr,
+    output logic [(DATA_WIDTH-1):0] dout
+);
+
+logic [(DATA_WIDTH-1):0] rom_content [(2**ADDR_WIDTH-1):0];
+
+initial begin
+    $display("Loading ROM content...");
+    $readmemh("sinerom.mem", rom_content);
+end
+
+always_ff @(posedge clk) begin
+    dout <= rom_content[addr];
+end
+
+endmodule
