74ba256482856661f0d4ba1d8e8b7c088f46572f
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-10-27 19:13:03 +0100

    Create counter.sv with step_size input. Using an `if` for `en` for clarity over the "embedded `en`" method

---
+++ task1/counter.sv
@@ -0,0 +1,17 @@
+module counter #(
+    parameter WIDTH = 8
+)(
+    input  logic [(WIDTH-1):0] step_size,
+    input  logic               clk,
+    input  logic               rst,
+    input  logic               en,
+    output logic               count
+);
+
+always_ff @(posedge clk) begin
+    if (rst)     count <= {WIDTH{1'b0}};
+    else if (en) count <= count + step_size;
+    else         count <= count;
+end
+
+endmodule
